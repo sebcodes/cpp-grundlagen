f6811d1431fc339a9a3ae0568fac9b65a22d607b
sebcodes <info@sebastian-kiefer.com>
2020-11-06 22:23:06 +0100

    Add ForLoop

---
+++ ForSchleife/ForSchleife/ForSchleife.cpp
@@ -0,0 +1,70 @@
+/*
+* Application "Eine Hilfe für alle die C++ nicht ganz verstehen"
+* Copyright (c) sebcodes 2020
+* @author sebcodes <info@sebcodes.de>
+* @see https://github.com/sebcodes/cpp-basycs
+*/
+
+#include <iostream>
+
+int main()
+{
+
+
+    /* Eine For-Schleife besteht aus 3 Bestandteilen, 
+       1. der lokalen Variablen Definition (Variable ist nur in der Schleife gültig)
+       2. der Bedingung zb. (i < 10)  -> i ist dabei die lokale variable
+       3. die Inkrementierung/Dekrementierung der Lokalen Variable zb. i++ (ist das selbe wie i = i+1)
+
+       Was macht die Schleife jetzt? Da die schleife ja noch einen Anweisungsblock besitzt "{}", wird dieser solange
+       wiederholt wie die bedingung (Bestandteil 2) wahr ist/erfüllt ist.
+       
+       Nach jedem Durchlauf wird Bestandteil 3 ausgeführt, im beispiel unten also immer "i" um 1 erhöht.
+
+    */
+
+    //Hier läuft die schleife 10 mal durch und "i" wird bei jedem Durchlauf um eins erhöht also 
+    // 1.Durchlauf: i = 0
+    // 2.Durchlauf: i = 1
+    // 3.Durchlauf: i = 2
+    // 4.Durchlauf: i = 3
+    // 5.Durchlauf: i = 4
+    // 6.Durchlauf: i = 5
+    // 7.Durchlauf: i = 6
+    // 8.Durchlauf: i = 7
+    // 9.Durchlauf: i = 8
+    // 10.Durchlauf: i = 9
+
+    std::cout << "Beispiel wo immer i +1  genommen wird:" << std::endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Durchlauf: " << i << std::endl;
+    }
+
+
+    /*
+    Beispiel wo immer i + 2 genommen wird:
+    */
+    std::cout << "Beispiel wo immer i + 2 genommen wird:" << std::endl;
+
+    //Durchlauf:  0
+    //Durchlauf:  2
+    //Durchlauf : 4
+    //Durchlauf : 6
+    //Durchlauf : 8
+
+    for (int i = 0; i < 10; i = i+2)
+    {
+        std::cout << "Durchlauf: " << i << std::endl;
+    }
+
+    /* 
+    wichtig : "i" oder wie ihr es nennt kann ich nur in der for verwenden, 
+    da dies eine lokale variable ist und sie ihren gültigkeitsbereich nur in der schleife hat, 
+    da sie sort definiert wird
+    */
+
+    (void)getchar();
+    return 0;
+}
